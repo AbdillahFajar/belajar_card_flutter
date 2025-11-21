3fe20e175cdfda448554ef16f47ffab46fae12c2
AbdillahFajar <fajarabdillah2264@gmail.com>
2025-11-21 09:00:38 +0700

    tambahin properti borderOnForeground yang kegunaanya belum diketahui lebih jelas

--- lib/pages/card_page_1.dart
+++ lib/pages/card_page_1.dart
@@ -22,7 +22,12 @@ class CardPage1 extends StatelessWidget {
                       elevation: 8, //bikin efek bayangan di card-nya
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(15.0), 
+                        side: BorderSide(
+                          color: Colors.black,
+                          width: 3.0
+                        ),
                       ),
+                      borderOnForeground: true, //ini buat apaan cuy?
                       child: Padding( //widget Padding, supaya bisa pakai properti padding kayak di bawah ini
                         padding: const EdgeInsets.all(8.0), //bikin spasi di sekitar tulisan/isi/konten di dalam card-nya
                         child: Text(
