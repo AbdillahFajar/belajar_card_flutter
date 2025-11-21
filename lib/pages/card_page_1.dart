7ebd6b26b9278df3010e1392e97c63753cd5ba5d
AbdillahFajar <fajarabdillah2264@gmail.com>
2025-11-21 09:10:33 +0700

    tambahin properti clipBehavior yang fungsinya juga belum diketahui

--- lib/pages/card_page_1.dart
+++ lib/pages/card_page_1.dart
@@ -16,24 +16,31 @@ class CardPage1 extends StatelessWidget {
                 padding: const EdgeInsets.all(20.0),
                 child: Column(
                   children: [
-                    Card(
-                      margin: EdgeInsets.all(16.0), //bikin jarak di sekitar card-nya (di luar card)
-                      color: Colors.amber,
-                      elevation: 8, //bikin efek bayangan di card-nya
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15.0), 
-                        side: BorderSide(
-                          color: Colors.black,
-                          width: 3.0
+                    Center(
+                      child: Card(
+                        //Menentukan bagaimana konten atau isi card dipotong dengan clipBehavior. Tapi, gak ada perbedaannya. Mungkin harus dihapusin yang gak perlu dulu
+                        // clipBehavior: Clip.none, //gak dipotong sama sekali kontennya,
+                        // clipBehavior: Clip.hardEdge, //kontennya dipotong sesuai border card-nya,
+                        // clipBehavior: Clip.antiAlias, //kontennya dipotong sesuai border card-nya dengan efek halus (anti aliasing),
+                        clipBehavior: Clip.antiAliasWithSaveLayer, //kontennya dipotong sesuai border card-nya dengan efek halus dan disimpan di layer terpisah,
+                        margin: EdgeInsets.all(16.0), //bikin jarak di sekitar card-nya (di luar card)
+                        color: Colors.amber,
+                        elevation: 8, //bikin efek bayangan di card-nya
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15.0), 
+                          side: BorderSide(
+                            color: Colors.black,
+                            width: 3.0
+                          ),
                         ),
-                      ),
-                      borderOnForeground: true, //ini buat apaan cuy?
-                      child: Padding( //widget Padding, supaya bisa pakai properti padding kayak di bawah ini
-                        padding: const EdgeInsets.all(8.0), //bikin spasi di sekitar tulisan/isi/konten di dalam card-nya
-                        child: Text(
-                          "Card warna amber",
-                          style: TextStyle(
-                            fontSize: 16.0
+                        borderOnForeground: true, //ini buat apaan cuy?
+                        child: Padding( //widget Padding, supaya bisa pakai properti padding kayak di bawah ini
+                          padding: const EdgeInsets.all(8.0), //bikin spasi di sekitar tulisan/isi/konten di dalam card-nya
+                          child: Text(
+                            "Card warna amber",
+                            style: TextStyle(
+                              fontSize: 16.0
+                            ),
                           ),
                         ),
                       ),
